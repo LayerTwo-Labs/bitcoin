1367e6ad45411110cc7183fb7d15eb5ab99474f9
CryptAxe <cryptaxe@gmail.com>
2024-01-12 20:00:55 -0800

    Update expected chain size for warnings

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -105,8 +105,8 @@ public:
         pchMessageStart[3] = 0xd9;
         nDefaultPort = 48883;
         nPruneAfterHeight = 100000;
-        m_assumed_blockchain_size = 590;
-        m_assumed_chain_state_size = 9;
+        m_assumed_blockchain_size = 1;
+        m_assumed_chain_state_size = 1;
 
         genesis = CreateGenesisBlock(1686349948, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
